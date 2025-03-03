cab882b95a738e8532dbf1850ae5b6d08ed9713a
use <use@DESKTOP-3QVJ858.mshome.net>
2025-03-03 13:17:20 +0530

    first push

---
+++ src/test/java/package1/ContactTest.java
@@ -0,0 +1,28 @@
+
+package package1;
+
+import org.testng.annotations.Test;
+
+public class ContactTest 
+{
+	@Test
+	public void createContactTest()
+	{
+		String URL=System.getProperty("url","http://localhost:8888");
+		String BROWSER=System.getProperty("browser","chrome");
+		String USERNAME=System.getProperty("username","admin");
+		String PASSWORD=System.getProperty("password","admin");
+		System.out.println("execute createContactTest");
+		System.out.println(URL);
+		System.out.println(BROWSER);
+		System.out.println(USERNAME);
+		System.out.println(PASSWORD);
+	}
+
+	@Test
+		public void modifyContactTest()
+	    {
+			System.out.println("execute modifyContactTest");
+		}
+	
+}
---
+++ src/test/java/package2/OrgTest.java
@@ -0,0 +1,22 @@
+package package2;
+
+import org.testng.annotations.Test;
+
+public class OrgTest 
+{
+	public class ContactTest
+	{
+		@Test
+		public void createOrgTest() 
+		{
+			System.out.println("execute createOrgTest");
+		}
+
+			@Test
+		public void modifyOrgTest()
+		{
+			System.out.println("execute modifyOrgTest");
+		}
+
+	}
+}
